446da3d7e483e7b1655f955eafb7a1d7f8a2e2e6
narendramamtora <narendramamtora97@gmail.com>
2023-06-29 19:07:06 +0530

    saving

--- getstarted/local/ram.js
+++ getstarted/local/ram.js
@@ -4,10 +4,32 @@
 function callvalue(){
     let myObj={
         name: document.getElementById("name").value,
-        emails: document.getElementById("email").value
+        email: document.getElementById("email").value,
+        number: document.getElementById("mnumber").value
     };
+    let eemail=document.getElementById("email").value
+
 
     let myObj_serialzed=JSON.stringify(myObj);
+    localStorage.setItem(eemail,myObj_serialzed)
+}       
 
-    localStorage.setItem("The Name and email",myObj_serialzed)
-    }      
+
+var myForm=document.querySelector('#my-form');
+var nameInput=document.querySelector('#name');
+var emailInput=document.querySelector('#email');
+var number =document.querySelector('#mnumber');
+var userList=document.querySelector('#users');
+
+myForm.addEventListener('submit', onsubmit);//if not working try onsubmit
+
+function onsubmit(e){
+    e.preventDefault();
+
+    const li=document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value} - ${emailInput.value} - ${number.value}`));    
+    userList.appendChild(li);
+    var a=nameInput.value
+    console.log(a);
+   
+    }
